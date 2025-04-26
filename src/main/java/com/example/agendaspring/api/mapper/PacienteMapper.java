9b7ec1e6fd5888aba8f6debd385ac1838d521c99
Arthurabriel <arthur.gabriel.santos.albuquerque@ccc.ufcg.edu.br>
2025-04-26 15:15:56 -0300

    Implementa MapStruct para Paciente

--- src/main/java/com/example/agendaspring/api/mapper/PacienteMapper.java
+++ src/main/java/com/example/agendaspring/api/mapper/PacienteMapper.java
@@ -3,33 +3,13 @@ package com.example.agendaspring.api.mapper;
 import com.example.agendaspring.api.dto.paciente.CreatePacienteDTO;
 import com.example.agendaspring.api.dto.paciente.PacienteDTO;
 import com.example.agendaspring.domain.entity.Paciente;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
-@Component
-public class PacienteMapper {
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface PacienteMapper {
 
-    public static PacienteDTO toPacienteDTO(Paciente paciente) {
-        if (paciente == null) {
-            return null;
-        }
+    PacienteDTO toDTO(Paciente paciente);
 
-        return PacienteDTO.builder()
-                .id(paciente.getId())
-                .nome(paciente.getNome())
-                .sobrenome(paciente.getSobrenome())
-                .endereco(paciente.getEndereco())
-                .email(paciente.getEmail())
-                .cpf(paciente.getCpf())
-                .build();
-    }
-
-    public static Paciente toPaciente(CreatePacienteDTO createPacienteDTO) {
-        return Paciente.builder()
-                .nome(createPacienteDTO.getNome())
-                .sobrenome(createPacienteDTO.getSobrenome())
-                .endereco(createPacienteDTO.getEndereco())
-                .email(createPacienteDTO.getEmail())
-                .cpf(createPacienteDTO.getCpf())
-                .build();
-    }
+    Paciente toPaciente(CreatePacienteDTO createPacienteDTO);
 }
--- src/main/java/com/example/agendaspring/domain/service/Paciente/PacienteServiceImpl.java
+++ src/main/java/com/example/agendaspring/domain/service/Paciente/PacienteServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.agendaspring.api.dto.paciente.UpdatePacienteDTO;
 import com.example.agendaspring.api.mapper.PacienteMapper;
 import com.example.agendaspring.domain.entity.Paciente;
 import com.example.agendaspring.domain.repository.PacienteRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,40 +15,39 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 @Transactional
 public class PacienteServiceImpl implements PacienteService {
 
     private final PacienteRepository pacienteRepository;
 
-    public PacienteServiceImpl(PacienteRepository pacienteRepository) {
-        this.pacienteRepository = pacienteRepository;
-    }
+    private final PacienteMapper pacienteMapper;
 
     public PacienteDTO create(CreatePacienteDTO createPacienteDTO) {
         if (pacienteRepository.existsByCpf(createPacienteDTO.getCpf())) {
             throw new IllegalArgumentException();
         }
-        Paciente paciente = PacienteMapper.toPaciente(createPacienteDTO);
+        Paciente paciente = pacienteMapper.toPaciente(createPacienteDTO);
         pacienteRepository.save(paciente);
-        return PacienteMapper.toPacienteDTO(paciente);
+        return pacienteMapper.toDTO(paciente);
     }
 
     public PacienteDTO get(Long id) {
         Paciente paciente = getPaciente(id);
-        return PacienteMapper.toPacienteDTO(paciente);
+        return pacienteMapper.toDTO(paciente);
     }
 
     public List<PacienteDTO> listar() {
         return pacienteRepository.findAll()
                 .stream()
-                .map(PacienteMapper::toPacienteDTO)
+                .map(pacienteMapper::toDTO)
                 .toList();
     }
 
     public PacienteDTO update(Long id, UpdatePacienteDTO updatePacienteDTO) {
         Paciente paciente = getPaciente(id);
         pacienteRepository.save(paciente);
-        return PacienteMapper.toPacienteDTO(paciente);
+        return pacienteMapper.toDTO(paciente);
     }
 
     public void remove(Long id) {
