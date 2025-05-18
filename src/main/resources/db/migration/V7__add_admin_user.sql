6412169b97e39b19df39fe22c9813f0c007eee7c
Arthurabriel <arthur.gabriel.santos.albuquerque@ccc.ufcg.edu.br>
2025-05-18 17:46:55 -0300

    Fix: add one more _

---
+++ src/main/resources/db/migration/V7__add_admin_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO USERS (login, password, role)
+VALUES (
+           'admin',
+           '$2a$10$UlVrnThvsbyWfw7yavT0R.4tjza/hFNeP213ha3cFRc9952Gs8PUK',
+           'ROLE_ADMIN'
+       );
